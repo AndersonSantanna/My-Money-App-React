a98528965fa1fced9e0c5e40f4a612a11328e5d5
AndersonSantanna <anderson.santanna14@gmail.com>
2018-12-19 22:37:43 -0200

    Fazendo alteraçoes no menu

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -9,9 +9,9 @@ export default props =>(
         </MenuTree>
         <MenuItem path='#/about' label='About' icon='info'/>
         <MenuTree label='Contats' icon='envelope'>
-            <MenuItem path='#/billingCycle' label='Facebook' icon='facebook'/>
-            <MenuItem path='#/billingCycle' label='Instagram' icon='instagram'/>
-            <MenuItem path='#/billingCycle' label='LinkedIn' icon='linkedin'/>
+            <MenuItem path='#/billingCycle' target='_black' label='Facebook' icon='facebook'/>
+            <MenuItem path='#/billingCycle' target='_black' label='Instagram' icon='instagram'/>
+            <MenuItem path='#/billingCycle' target='_black' label='LinkedIn' icon='linkedin'/>
             <MenuItem path='https://github.com/andersonSantanna' target='_black' label='Github' icon='github'/>
         </MenuTree>
     </ul>
